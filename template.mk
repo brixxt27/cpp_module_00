ac1b9c4b8ef8ebfcdb766c7d57bd283e6589f0c2
“brixxt27” <brixxt27@gmail.com>
2022-12-12 15:02:54 +0900

    feat: makefile 및 template.mk 작성 완료

---
+++ template.mk
@@ -0,0 +1,54 @@
+DIR_INCS	= ./
+OBJS		= $(SRCS:.cpp=.o)
+
+# =============================================================================
+# Command Variables
+# =============================================================================
+
+CC			= c++
+RM			= rm -rf
+ifdef DEBUG
+	CFLAGS	= --std=c++98 -g3 -fsanitize=address
+else ifdef LEAKS
+	CFLAGS	= --std=c++98 -g3
+else
+	CFLAGS	= --std=c++98 -Wall -Wextra -Werror
+endif
+
+# =============================================================================
+# Target Generating
+# =============================================================================
+
+%.o		: %.cpp
+	$(CC) $(CFLAGS) -I $(DIR_INCS) -c $< -o $@
+
+$(NAME)	: $(OBJS) $(INCS)
+	$(CC) $(CFLAGS) -I $(DIR_INCS) $(OBJS) -o $(NAME)
+
+# =============================================================================
+# Rules
+# =============================================================================
+
+.PHONY	: all
+all		: $(NAME)
+
+.PHONY	: clean
+clean	:
+	$(RM) $(OBJS)
+
+.PHONY	: fclean
+fclean	: clean
+	$(RM) $(NAME)
+
+.PHONY	: re
+re		:
+	make fclean
+	make all
+
+.PHONY	: debug
+debug	:
+	make DEBUG=1 all
+
+.PHONY	: leaks
+leaks	:
+	make LEAKS=1 all
