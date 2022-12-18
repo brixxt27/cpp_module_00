4111ab64b8c6088574f97bf24e6b7ff57879b77c
brixxt27 <brixxt27@gmail.com>
2022-12-18 12:48:31 +0900

    feat: streamstring 을 사용했지만 변환이 atoi처럼 되어 문자가 오면 0이 됨

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -9,15 +9,22 @@ void	PhoneBook::Add() {
 }
 
 void	PhoneBook::Search() const {
-	std::string str;
-	std::streamstring ss;
+	std::string				str;
+	std::stringstream	sstr;
+	int								idx;
 
 	std::cout << "*** Input an integer index between 0 and 8 ***" << std::endl;
 	std::cin >> str;
+
+	sstr << str;
+	sstr >> idx;
+
+	std::cout << "idx: " << idx << std::endl;
+
 	if (idx >= 0 && idx <= 8) {
 		std::cout
 		<< std::setw(10) << std::right << "index" << "|"
-		<< std::setw(10) << std::right << "first name" << "|"
+		<< std::setw(10) << std::right << "first name" << "|tr"
 		<< std::setw(10) << std::right << "last name" << "|"
 		<< std::setw(10) << std::right << "nickname" << std::endl;
 	}
