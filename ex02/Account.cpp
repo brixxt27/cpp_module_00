bb4bd34206e51631d9a667fdd95d7ea1e3f2a2bb
brixxt27 <brixxt27@gmail.com>
2022-12-24 03:19:03 +0900

    feat: 생성자 구현 및 c++ 문법 친숙해 짐#13
    - static 멤버 변수는 기본으로 0으로 초기화 되지 않는다. 애초에 메모리를
    할당 받지 않는다. 헤더에 적기만 하는 행위는 정의만 되어 있는 것이다.
    그래서 직접 사용하려면 전역 변수처럼 선언 및 초기화를 해줘야 한다.
    static 멤버 변수는 같은 클래스의 개체라면 누구든지 같은 것을 사용할 수
    있다.
    - 기본 생성자인 ClassName()을 private에 두면 컴파일러는 기본 생성자를
    자동으로 만들지 않는다.

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,10 +1,36 @@
 #include "Account.hpp"
 #include <iostream>
 
-Account::Account( int initial_deposit ) {
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account( int initial_deposit ) 
+: _accountIndex(_nbAccounts){
 	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << initial_deposit << ";created" << std::endl;
 }
 
 void	Account::_displayTimestamp( void ) {
 
 }
+
+int	Account::getNbAccounts( void ) {
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return _totalNbWithdrawals;
+}
+
+// void	Account::displayAccountsInfos( void ) {}
+
--- ex02/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int	main() {
-	int	const	amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
-
-	std::cout << amounts << std::endl;
-	std::cout << amounts + 8 << std::endl;
-}
